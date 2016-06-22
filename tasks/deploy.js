4cb314ed8b997e0b7d3396f3b98efed107a19f91
Luke <loo2km@gmail.com>
2016-06-22 17:06:24 +0800

    [*] allow user keep ftp pass empty, make user type password while call deploy task

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -8,19 +8,24 @@ var combiner = require('stream-combiner2');
 var vfs      = require('vinyl-fs');
 var vftp     = require('vinyl-ftp');
 var vpath    = require('vinyl-paths');
+var readline = require('readline-sync');
 
 module.exports = function(gulp, $, conf) {
-    // ftp 配置信息
-    var ftpSvr = vftp.create({
-        host     : conf.ftp.host,
-        user     : conf.ftp.user,
-        password : conf.ftp.pass,
-        port     : conf.ftp.port,
-        parallel : 5,
-        log      : $.util.log,
-    });
-
     gulp.task('deploy', function() {
+        if (conf.ftp.pass === '') {
+            conf.ftp.pass = readline.question('Type your ftp server\'s password: ');
+        }
+
+        // ftp 配置信息
+        var ftpSvr = vftp.create({
+            host     : conf.ftp.host,
+            user     : conf.ftp.user,
+            password : conf.ftp.pass,
+            port     : conf.ftp.port,
+            parallel : 5,
+            log      : $.util.log,
+        });
+
         vfs.src([conf.parsePwd([conf.dist, '/**'].join(''))], { buffer: false })
             .pipe(ftpSvr.dest(conf.ftp.path));
     });
