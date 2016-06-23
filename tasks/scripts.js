5f019117899f92c5e6d0072bc4fbfc736449b161
Luke <loo2km@gmail.com>
2016-06-23 11:30:57 +0800

    [!] fix dest dir

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -7,11 +7,11 @@
 module.exports = function(gulp, $, conf, browserSync) {
     gulp.task('scripts',function() {
         var compress = conf.build.compress !== true ? !!conf.build.compress.js : true;
-        return gulp.src(conf.parsePwd(conf.scriptFiles))
+        return gulp.src(conf.parsePwd(conf.scriptFiles), { base: conf.parsePwd(conf.app) })
             .pipe($.if(!conf.isProduction, $.sourcemaps.init()))
             .pipe($.if(conf.isProduction && compress, $.uglify()))
             .pipe($.if(!conf.isProduction, $.sourcemaps.write()))
-            .pipe(gulp.dest(conf.parsePwd([conf.tmp, '/js'].join(''))))
+            .pipe(gulp.dest(conf.parsePwd(conf.tmp)))
             .pipe(browserSync.stream());
     });
 }
--- tasks/styles.js
+++ tasks/styles.js
@@ -7,13 +7,13 @@
 module.exports = function(gulp, $, conf, browserSync) {
     gulp.task('styles', function() {
         var compress = conf.build.compress !== true ? !!conf.build.compress.css : true;
-        return gulp.src(conf.parsePwd(conf.styleFiles))
+        return gulp.src(conf.parsePwd(conf.styleFiles), { base: conf.parsePwd(conf.app) })
             .pipe($.if(!conf.isProduction, $.sourcemaps.init()))
             .pipe($.less()).on('error', conf.errorHandler('Less'))
             .pipe($.autoprefixer()).on('error', conf.errorHandler('Autoprefixer'))
             .pipe($.if(conf.isProduction && compress, $.cleanCss({compatibility: 'ie7'})))
             .pipe($.if(!conf.isProduction, $.sourcemaps.write()))
-            .pipe(gulp.dest(conf.parsePwd([conf.tmp, '/css'].join(''))))
+            .pipe(gulp.dest(conf.parsePwd(conf.tmp)))
             .pipe(browserSync.stream());
     });
 }
