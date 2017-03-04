c7def75f916b1ea14f331dacffa4cdfa08cb97db
Luke <loo2km@gmail.com>
2017-03-04 14:51:42 +0800

    [+] add less-plguin-est support

--- tasks/styles.js
+++ tasks/styles.js
@@ -4,12 +4,15 @@
  * @date 2016-03-24
  */
 
+est = require('less-plugin-est');
+estPlugin = new est();
+
 module.exports = function(gulp, $, conf, browserSync) {
     gulp.task('styles', function() {
         var compress = conf.build.compress !== true ? !!conf.build.compress.css : true;
         return gulp.src(conf.parsePwd(conf.styleFiles), { base: conf.parsePwd(conf.app) })
             .pipe($.if(!conf.isProduction, $.sourcemaps.init()))
-            .pipe($.less())
+            .pipe($.less({plugins: [estPlugin]}))
                 .on('error', conf.errorHandler('Less'))
             .pipe($.autoprefixer({ cascade: false }))
                 .on('error', conf.errorHandler('Autoprefixer'))
